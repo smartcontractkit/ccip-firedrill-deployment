0d456f84117cd9324dbea15ad0740d4b9f7f69ec
Aleksandr Bukata <aleksandr.bukata@smartcontract.com>
2025-04-18 14:51:47 +0100

    solana tx

--- deployment/solana/exec_drill.go
+++ deployment/solana/exec_drill.go
@@ -1,9 +1,12 @@
 package solana
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/gagliardetto/solana-go"
+	solRpc "github.com/gagliardetto/solana-go/rpc"
+	solCommonUtil "github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/common"
 	"github.com/smartcontractkit/chainlink/deployment"
 
 	"github.com/smartcontractkit/ccip-firedrill-deployment/chains/solana/gobindings/firedrill_offramp"
@@ -11,7 +14,7 @@ import (
 	"github.com/smartcontractkit/ccip-firedrill-deployment/deployment/shared"
 )
 
-func CallDrillPrepareRegister(client deployment.SolChain, view shared.FiredrillEntrypointView) error {
+func CallDrillPrepareRegister(client deployment.SolChain, view shared.FiredrillEntrypointView) (*solana.Transaction, error) {
 	offRampProgram := solana.MustPublicKeyFromBase58(view.OffRamp)
 	firedrill_offramp.SetProgramID(offRampProgram)
 	offRampPDA, _, _ := FindFiredrillOnrampPDA(offRampProgram)
@@ -20,16 +23,16 @@ func CallDrillPrepareRegister(client deployment.SolChain, view shared.FiredrillE
 		client.DeployerKey.PublicKey(),
 	).ValidateAndBuild()
 	if err != nil {
-		return fmt.Errorf("error building prepare register instruction: %w", err)
+		return nil, fmt.Errorf("error building prepare-register instruction: %w", err)
 	}
-	err = client.Confirm([]solana.Instruction{ix})
+	tx, err := Confirm(client, ix)
 	if err != nil {
-		return fmt.Errorf("error confirming prepare register instruction: %w", err)
+		return nil, fmt.Errorf("error confirming prepare-register instruction: %w", err)
 	}
-	return nil
+	return tx, nil
 }
 
-func CallDrillPendingCommit(idx uint64, client deployment.SolChain, view shared.FiredrillEntrypointView) error {
+func CallDrillPendingCommit(idx uint64, client deployment.SolChain, view shared.FiredrillEntrypointView) (*solana.Transaction, error) {
 	onRampProgram := solana.MustPublicKeyFromBase58(view.OnRamp)
 	onRampPDA, _, _ := FindFiredrillOnrampPDA(onRampProgram)
 	firedrill_onramp.SetProgramID(onRampProgram)
@@ -40,16 +43,16 @@ func CallDrillPendingCommit(idx uint64, client deployment.SolChain, view shared.
 		client.DeployerKey.PublicKey(),
 	).ValidateAndBuild()
 	if err != nil {
-		return fmt.Errorf("error building drill pending commit instruction: %w", err)
+		return nil, fmt.Errorf("error building pending-commit instruction: %w", err)
 	}
-	err = client.Confirm([]solana.Instruction{ix})
+	tx, err := Confirm(client, ix)
 	if err != nil {
-		return fmt.Errorf("error confirming drill pending commit instruction: %w", err)
+		return nil, fmt.Errorf("error confirming pending-commit instruction: %w", err)
 	}
-	return nil
+	return tx, nil
 }
 
-func CallDrillPendingExec(from uint64, to uint64, client deployment.SolChain, view shared.FiredrillEntrypointView) error {
+func CallDrillPendingExec(from uint64, to uint64, client deployment.SolChain, view shared.FiredrillEntrypointView) (*solana.Transaction, error) {
 	offRampProgram := solana.MustPublicKeyFromBase58(view.OffRamp)
 	firedrill_offramp.SetProgramID(offRampProgram)
 	offRampPDA, _, _ := FindFiredrillOnrampPDA(offRampProgram)
@@ -59,11 +62,25 @@ func CallDrillPendingExec(from uint64, to uint64, client deployment.SolChain, vi
 		client.DeployerKey.PublicKey(),
 	).ValidateAndBuild()
 	if err != nil {
-		return fmt.Errorf("error building prepare register instruction: %w", err)
+		return nil, fmt.Errorf("error building pending-exec instruction: %w", err)
 	}
-	err = client.Confirm([]solana.Instruction{ix})
+	tx, err := Confirm(client, ix)
 	if err != nil {
-		return fmt.Errorf("error confirming prepare register instruction: %w", err)
+		return nil, fmt.Errorf("error confirming pending-exec instruction: %w", err)
 	}
-	return nil
+	return tx, nil
+}
+
+func Confirm(chain deployment.SolChain, ix solana.Instruction) (*solana.Transaction, error) {
+	txReceipt, err := solCommonUtil.SendAndConfirm(
+		context.Background(), chain.Client, []solana.Instruction{ix}, *chain.DeployerKey, solRpc.CommitmentConfirmed,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error confirming instruction: %w", err)
+	}
+	tx, err := txReceipt.Transaction.GetTransaction()
+	if err != nil {
+		return nil, fmt.Errorf("error getting transaction: %w", err)
+	}
+	return tx, nil
 }
