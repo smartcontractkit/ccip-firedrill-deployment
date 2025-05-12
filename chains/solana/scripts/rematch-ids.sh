21b1fa30f6f44ebe4af7a175105d9b0c7b0de207
Aleksandr Bukata <aleksandr.bukata@smartcontract.com>
2025-05-12 11:40:54 +0100

    rematch ids

--- chains/solana/scripts/rematch-ids.sh
+++ chains/solana/scripts/rematch-ids.sh
@@ -2,6 +2,7 @@
 
 for dir in contracts/programs/*; do
   name=$(basename "$dir")
+  toml_path="contracts/Anchor.toml"
   keypair_path="contracts/target/deploy/${name}-keypair.json"
   program_path="${dir}/src/lib.rs"
 
@@ -16,6 +17,9 @@ for dir in contracts/programs/*; do
 
   # Update declare_id!
   sed -i '' -E "s@declare_id!\(\".*\"\)@declare_id!(\"$new_id\")@" "$program_path"
+
+  # Update Anchor.toml
+  sed -i '' -E "s@$name = \".*\"@$name = \"$new_id\"@" "$toml_path"
 
   echo "Updated $name to $new_id"
 done
